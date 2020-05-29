a69240e888e15a9f78e5c5be6d14f68fff289350
Eva Achterhold <eva.achterhold@campus.lmu.de>
2020-05-29 10:08:23 +0200

    Add Podfile

---
+++ Pods/Target Support Files/Pods-Schnitzeljagd-SchnitzeljagdUITests/Pods-Schnitzeljagd-SchnitzeljagdUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Schnitzeljagd_SchnitzeljagdUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Schnitzeljagd_SchnitzeljagdUITests
+@end
--- Schnitzeljagd/ARUIView.swift
+++ Schnitzeljagd/ARUIView.swift
@@ -11,16 +11,28 @@ import SwiftUI
 struct ARUIView: View {
     @EnvironmentObject var data: DataModel
     var body: some View {
-        List {
-            Toggle(isOn: $data.enableAR) {
-                Text("AR")
-            }
+    VStack {
+        Button(action: {
+            self.data.enableAR.toggle()
+        }) {
+            Text("AR")
+                .fontWeight(.bold)
+                .font(.title)
+                .padding()
+                .background(Color.blue)
+                .cornerRadius(40)
+                .foregroundColor(.white)
+                .padding(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 40)
+                        .stroke(Color.purple, lineWidth: 5)
+                )
+        }.padding()
 //            Stepper("X: \(Int(data.xTranslation))", value:$data.xTranslation, in: -100...100)
 //            Stepper("y: \(Int(data.yTranslation))", value:$data.yTranslation, in: -100...100)
 //            Stepper("XZ \(Int(data.zTranslation))", value:$data.zTranslation, in: -100...100)
-        }
-        .frame(width: CGFloat(200))
     }
+}
 }
 
 struct ARUIView_Previews: PreviewProvider {
